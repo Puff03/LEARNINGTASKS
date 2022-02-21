0ac9a24e534bb9788a89c20a8309770470461584
Puff03 <100128364+Puff03@users.noreply.github.com>
2022-02-21 19:02:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 </head>
 <body> 
 <h1> Welcome to My Website! I am Meco P Tabjan!</h1>
-<ul>
+<ol>
 <li><a href="index.php">HOME</a><li>
 <li><a href="LEARNING TASK 2.html">LEARNING TASK 2</a><li>
-</ul>
+</ol>
 </body>
 </html>
