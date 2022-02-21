d48924f3bcb69155c7dbf7bec1121b6d5b371f79
Puff03 <100128364+Puff03@users.noreply.github.com>
2022-02-21 18:49:25 +0800

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Home Page</title>
+</head>
+<body> 
+<h1> Welcome to My Website! I am Meco P Tabjan!</h1>
+<ul>
+<li><a href="index1.php">HOME</a><li>
+<li><a href="/LEARNING TASK 2.html">LEARNING TASK 2</a><li>
+</ul>
+</body>
+</html>
