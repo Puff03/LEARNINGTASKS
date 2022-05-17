f7e0c0e6ab2f7f96cbfee30aa2fc2430440e3f5b
Puff03 <100128364+Puff03@users.noreply.github.com>
2022-05-17 23:26:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <li><a href="LEARNING TASK 3.html">LEARNING TASK 3</a><li>
 <li><a href="CLE1MT.html">CLE1MT</a></li>
  <li><a href="LT4.html">LT4</a></li>
-  <li><a href="LT5.html">LT5</a></li>
+  <li><a href="LT5">LT5</a></li>
   
 </body>
 </html>
