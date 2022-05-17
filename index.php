2369674bc38f752ba70d2d3464d10cb519a6f107
Puff03 <100128364+Puff03@users.noreply.github.com>
2022-05-18 02:42:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>My Home Page</title>
+            <link rel="stylesheet" 
+            type="text/css"
+            href="Stylesheet6.css">
+                }
+     <script type="text/javascript">
+                function togglemenu(){
+                    document.getElementById('sidebar').classList.toggle('active');
+                }
+    </script>
+    <title>My Home Page</title>
 </head>
 <body> 
-<h1> Welcome to My Website! I am Meco P Tabjan!</h1>
-
-<li><a href="index.php">HOME</a><li>
-<li><a href="LEARNING TASK 2.html">LEARNING TASK 2</a><li>
-<li><a href="LEARNING TASK 3.html">LEARNING TASK 3</a><li>
-<li><a href="CLE1MT.html">CLE1MT</a></li>
- <li><a href="LT4.html">LT4</a></li>
-  <li><a href="LT5">LT5</a></li>
+<div id="wrapper" onclick="togglemenu()">
+     <h1>Hello Welcome to My Website! I am Meco P Tabjan</h1>
+    <div id="sidebar">
+        <div class="toggle-btn">
+        <span></span>
+        <span></span>
+        <span></span>
+        </div>
+   <ul>
+       <li><a href="index.php">HOME </a><li>
+        <li><a href="CLE1MT.html">CLE1 MIDTERM </a></li>
+        <li><a href="LEARNING TASK 2.html">LEARNING TASK 2</a></li>
+        <li><a href="LEARNING TASK 3.html">LEARNING TASK 3</a></li>
+       <li><a href="LT4.html">LEARNING TASK 4</a></li>
+       <li><a href="LT5">LEARNING TASK 5</a></li>
+     
+    </ul>
+    </div>
+</div>
+    
   
 </body>
 </html>
