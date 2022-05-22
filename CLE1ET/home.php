bf6c03dd98072dd37da30c59dfd4a2c83451b583
Puff03 <100128364+Puff03@users.noreply.github.com>
2022-05-22 20:57:49 +0800

    Update home.php

--- CLE1ET/home.php
+++ CLE1ET/home.php
@@ -1,8 +1,9 @@
 <link rel="stylesheet" href="style2.css">
-<script><?php include('index.php'); ?></script>
+
 <div id="wrapper">
 
     <h1>Hello WELCOME TO MY WEBSITE<br>I AM
         
         <br> Meco Tabjan</h1>
 </div>
+<script><?php include('index.php'); ?></script>
