af011a0d98268f7d0b74d6e82575cd0a47734302
Puff03 <100128364+Puff03@users.noreply.github.com>
2022-05-22 20:59:13 +0800

    Update projects.php

--- CLE1ET/projects.php
+++ CLE1ET/projects.php
@@ -1,29 +1,19 @@
-
-
-       
-    <link rel="stylesheet" 
+  <link rel="stylesheet" 
           href="style2.css">
-    <script><?php include('index.php'); ?></script>
-    
-
+<body>
     <div id="wrapper">
-    
         <div id="boxA">
             <div id="namebox">
                 <h1>LT2</h1>
                 <div id="view"><h1><a href="https://puff1.herokuapp.com/LEARNING%20TASK%202.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
             </div>
             <img alt="pic" src="images/lt2.png"></div>
-        
-        
         <div id="boxA">
             <div id="namebox">
                 <h1>LT3</h1>
                 <div id="view"><h1><a href="https://puff1.herokuapp.com/LEARNING%20TASK%203.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
             </div>
             <img alt="pic" src="images/lt3.png">
-            
-            
         </div>
         <div id="boxA">
            <div id="namebox">
@@ -34,14 +24,12 @@
                  width=210px;
                  height=50px;>
             </div>
-            
         <div id="boxA">
             <div id="namebox">
                 <h1>CLE1MT</h1>
                 <div id="view"><h1><a href="https://puff1.herokuapp.com/CLE1MT.html" target="_blank" rel="noopener noreferrer">view</a></h1></div>
             </div>
                 <img alt="pic" src="images/cle1.png">
-            
         </div>
         <div id="boxA">
             <div id="namebox">
@@ -49,10 +37,11 @@
                 <div id="view"><h1><a href="https://github.com/Puff03/LEARNINGTASKS/tree/main/LT5" target="_blank" rel="noopener noreferrer">view</a></h1></div>
             </div>
             <img alt="pic" src="images/LT5.png">
-        
         </div>
-        
     </div>
+    <script><?php include('index.php'); ?></script>
+</body>
+ 
  
     
 
